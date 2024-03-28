eaad43921779416d1ef4f8a3c4dc540581ce22ab
HoHuuCuong <hohuucuong44@gmail.com>
2024-03-28 15:59:38 +0700

    add

---
+++ AsoftTest/AsoftTest/Controllers/UserController.cs
@@ -0,0 +1,122 @@
+﻿using AsoftTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity.Validation;
+
+namespace AsoftTest.Controllers
+{
+    public class UserController : Controller
+    {
+        private testEntities db = new testEntities();
+        // GET: User
+        public ActionResult Index()
+        {
+            var listUser = new testEntities().Tests.ToList();
+            return View(listUser);
+        }
+      
+        public ActionResult ThemUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ThemUser(Test user)
+        {
+            if (ModelState.IsValid)
+            {
+                bool checkID = db.Tests.Any(u => u.UserID == user.UserID);
+                if (checkID)
+                {
+                    ModelState.AddModelError("", "UserID đã tồn tại.");
+                    return RedirectToAction("Index");
+                }
+                db.Tests.Add(user);
+                db.SaveChanges(); 
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                return View(user);
+            }
+
+        }
+        [HttpGet]
+        public ActionResult GetUserByID(string userID)
+        {
+            var user = db.Tests.FirstOrDefault(u => u.UserID == userID);
+
+            if (user != null)
+            {
+                // Trả về dữ liệu người dùng dưới dạng JSON
+                return Json(user, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                // Nếu không tìm thấy người dùng, trả về null
+                return Json(null, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Bảo vệ khỏi các cuộc tấn công CSRF
+        public ActionResult SuaUser(Test user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                try
+                {
+                   
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "User");
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    // Retrieve the error messages as a list of strings.
+                    var errorMessages = ex.EntityValidationErrors
+                            .SelectMany(x => x.ValidationErrors)
+                            .Select(x => x.ErrorMessage);
+
+                    // Join the list to a single string.
+                    var fullErrorMessage = string.Join("; ", errorMessages);
+
+                    // Combine the original exception message with the new one.
+                    var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                    // Throw a new DbEntityValidationException with the improved exception message.
+                    throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+                }
+            }
+           
+           return View(user);
+        }
+
+
+        public ActionResult XoaUser(string userID)
+        {
+          
+            var user = db.Tests.FirstOrDefault(u => u.UserID == userID);
+            if (user == null)
+            {
+                return RedirectToAction("Index", "User");
+            }
+
+            try
+            {
+                db.Tests.Remove(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "User");
+            }
+        }
+
+
+    }
+}
---
+++ AsoftTest/AsoftTest/Scripts/User.js
@@ -0,0 +1,116 @@
+﻿
+function checkPasswordMatch() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    var errorDiv = document.getElementById("passwordMatchError");
+
+    if (password !== confirmPassword) {
+        errorDiv.innerHTML = "Mật khẩu không trùng khớp.";
+    } else {
+        errorDiv.innerHTML = "";
+    }
+}
+$(document).ready(function () {
+    $('#saveButton').click(function () {
+
+        // Kiểm tra mã người dùng
+        var userId = $('#userId').val().trim();
+        if (userId === '') {
+            alert('Vui lòng nhập mã nhân viên.');
+            return false;
+        }
+        var userName = $('#userName').val().trim();
+        if (userName === '') {
+            alert('Vui lòng nhập tên nhân viên.');
+            return false;
+        }
+        var password = $('#password').val().trim();
+        var confirmPassword = $('#confirmPassword').val().trim();
+        if (password !== '' || confirmPassword !== '') {
+            if (password !== confirmPassword) {
+                alert('Mật khẩu nhập lại không khớp.');
+                return false;
+            }
+        }
+        var email = $('#email').val().trim();
+        var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (!filter.test(email)) {
+            alert('Email không hợp lệ.');
+            return false;
+        }
+
+    });
+
+
+
+    $('.editUser').click(function () {
+        var userID = $(this).data('userid');
+        console.log(userID);
+        $('#myModalUd').find('.modal-title').text('Sửa thông tin người dùng');
+        $('#myModalUd').find('button[type="submit"]').show();
+        $('#myModalUd').find('input').not('#editUserID').prop('readonly', false);
+        $.ajax({
+            url: '/User/GetUserByID',
+            type: 'GET',
+            data: { userID: userID },
+            success: function (response) {
+                // Điền dữ liệu vào form sửa
+                $('#editUserID').val(response.UserID);
+                $('#editUserName').val(response.UserName);
+                $('#editEmail').val(response.Email);
+                $('#editPassword').val(response.Password);
+                $('#editTel').val(response.Tel);
+            },
+            error: function () {
+                alert('Có lỗi xảy ra khi lấy dữ liệu người dùng.');
+            }
+        });
+    });
+
+
+    $('.deleteUser').click(function () {
+        var button = $(this);
+        if (confirm("Bạn có muốn xóa không?")) {
+            var userID = $(this).data('userid');
+            $.ajax({
+                url: '/User/XoaUser',
+                type: 'POST', 
+                data: { userID: userID },
+                success: function (response) {
+                    alert('Xóa thành công');
+                    button.closest('tr').remove();
+                },
+                error: function () {
+                    alert('Đã xảy ra lỗi khi xóa người dùng');
+                }
+            });
+        }
+    });
+    $('.detailUser').click(function () {
+        // Thay đổi tiêu đề form
+        $('#myModalUd').find('.modal-title').text('Xem thông tin người dùng');
+        // Ẩn nút Lưu
+        $('#myModalUd').find('button[type="submit"]').hide();
+        // Thêm thuộc tính readonly cho các ô input
+        $('#myModalUd').find('input').prop('readonly', true);
+        var userID = $(this).data('userid');
+        console.log(userID);
+        $.ajax({
+            url: '/User/GetUserByID',
+            type: 'GET',
+            data: { userID: userID },
+            success: function (response) {
+                $('#editUserID').val(response.UserID);
+                $('#editUserName').val(response.UserName);
+                $('#editEmail').val(response.Email);
+                $('#editPassword').val(response.Password);
+                $('#editTel').val(response.Tel);
+            },
+            error: function () {
+                alert('Có lỗi xảy ra khi lấy dữ liệu người dùng.');
+            }
+        });
+    });
+
+
+});
